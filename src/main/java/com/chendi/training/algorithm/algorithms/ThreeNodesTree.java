c8e12f5a0a55691359f1fc8a563a7a526295918e
Di Chen <chendi1025@gmail.com>
2019-01-29 18:21:13 -0500

    BFS  tree

---
+++ src/main/java/com/chendi/training/algorithm/algorithms/ThreeNodesTree.java
@@ -0,0 +1,76 @@
+package com.chendi.training.algorithm.algorithms;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class ThreeNodesTree {
+    int value;
+    ThreeNodesTree left;
+    ThreeNodesTree right;
+    ThreeNodesTree middle;
+
+    public ThreeNodesTree(){}
+
+    public ThreeNodesTree(int node){
+        this.value=node;
+    }
+
+    public static void threeNodesTreeToQueue(ThreeNodesTree rootnode){
+        List<ThreeNodesTree> list = new ArrayList<>();
+        Queue<List> myQueue = new ArrayDeque<>();
+
+        list.add(rootnode);
+
+        while (list.size()>0){
+            List<ThreeNodesTree> copy = new ArrayList<>(list);
+            list = new ArrayList<>();
+
+            for (ThreeNodesTree n: copy){
+                if (n.left!=null){
+                    list.add(n.left);
+                }
+                if(n.middle!=null){
+                    list.add(n.middle);
+                }
+                if(n.right!= null){
+                    list.add(n.right);
+                }
+            }
+            myQueue.offer(list);
+        }
+    System.out.println(myQueue);
+    }
+
+    public static void main(String []args){
+        ThreeNodesTree root = new ThreeNodesTree(1);
+        ThreeNodesTree l = new ThreeNodesTree(2);
+        ThreeNodesTree m = new ThreeNodesTree(3);
+        ThreeNodesTree r = new ThreeNodesTree(4);
+        ThreeNodesTree ll = new ThreeNodesTree(5);
+        ThreeNodesTree lm = new ThreeNodesTree(6);
+        ThreeNodesTree lr = new ThreeNodesTree(7);
+        ThreeNodesTree ml = new ThreeNodesTree(8);
+        ThreeNodesTree mm = new ThreeNodesTree(9);
+        ThreeNodesTree mr = new ThreeNodesTree(10);
+        ThreeNodesTree rl = new ThreeNodesTree(11);
+        ThreeNodesTree rm = new ThreeNodesTree(12);
+        ThreeNodesTree rr = new ThreeNodesTree(13);
+        root.left=l;
+        root.middle=m;
+        root.right=r;
+        l.left=ll;
+        l.middle=lm;
+        l.right=lr;
+        m.left=ml;
+        m.middle=mm;
+        m.right=mr;
+        r.right=rr;
+        r.middle=rm;
+        r.left=rl;
+        threeNodesTreeToQueue(root);
+    }
+
+
+}
